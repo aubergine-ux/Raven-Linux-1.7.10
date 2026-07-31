b4bf7c4d66d47d6e390b5c8a3d10be91d9686964
ruby <126033183+aubergine-ux@users.noreply.github.com>
2026-07-31 10:45:28 -0400

    v26.1 | Port to Linux

--- buildJar.sh
+++ buildJar.sh
@@ -1,21 +1,17 @@
-echo off
-cls
+#!/bin/bash
 echo "A Java 8 installation is required to build."
 echo "If you have a Java 8 installation, but your JAVA_HOME is set to something else,"
 echo "please set it to your Java 8 installation."
-echo "   "
+echo ""
 echo "Current JAVA_HOME version:"
 java -version
-echo "   "
+echo ""
 echo "Please only continue if your Java version is 8."
-echo "   "
-pause
-cls
+echo ""
+read -p "Press enter to continue..."
 echo "You can find the jar in /build/libs once the build is done."
-echo "   "
-pause
-cls
-cls
-.\\gradlew clean build
-echo "   "
-pause
+echo ""
+read -p "Press enter to start build..."
+./gradlew clean build
+echo ""
+read -p "Press enter to exit..."
